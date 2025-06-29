4c5a57455b47252b17c49188b0d38d172f31c45a
sektant-gabe <65908248+sektant-gabe@users.noreply.github.com>
2025-06-29 15:54:02 -0300

    D: fixed some boot.dev tests due to wrinting the entire program in one go instead of following the site

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ def get_book_text(filepath):
         print(f"Analyzing book found at {filepath}...")
         with open(filepath, encoding="utf-8") as f:
             data = f.read()
+            words = data.split()
+            print(f"{len(words)} words found in the document")
             letters = list(data.lower())
             letter_count = {}
-            print(f"Found {len(letters) + 1} total words")
+            print(f"Found {len(letters) + 1} total letters")
             for letter in letters:
                 if letter.isalpha():
                     if letter in letter_count.keys():
@@ -21,7 +23,7 @@ def get_book_text(filepath):
                 print(f"{letter.upper()}: {count}")
             print("------------------------ END ----------------------------")
 def main():
-    print("====================== BOOKBOT ==========================")
+    print("====================== BOOKBOT Frankenstein; Or, The Modern Prometheus ==========================")
     get_book_text("books/frankenstein.txt")
     return
 
