0ed8bc78b343599a42d0af2d72e706a9701bd461
sektant-gabe <65908248+sektant-gabe@users.noreply.github.com>
2025-06-29 17:22:18 -0300

    D: kinda finished

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
+from stats import get_word_count, get_chars_count, sort_chars
+from sys import argv, exit
+
+
+if len(argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    exit(1)
+
 def get_book_text(filepath):
     if type(filepath) != str:
         return "wrong type, must use string for path"
     else:
         print(f"Analyzing book found at {filepath}...")
-        with open(filepath, encoding="utf-8") as f:
-            data = f.read()
-            words = data.split()
-            print(f"{len(words)} words found in the document")
-            letters = list(data.lower())
-            letter_count = {}
-            print(f"Found {len(letters) + 1} total letters")
-            for letter in letters:
-                if letter.isalpha():
-                    if letter in letter_count.keys():
-                        letter_count[letter] += 1
-                    if letter not in letter_count.keys():
-                        letter_count.update({f"{letter}": 1})
-            sorted_items = sorted(letter_count.items(), key=lambda item: item[1], reverse=True)
-            sorted_dictionary_asc = dict(sorted_items)
-            print("------------------ Character Count ----------------------")
-            for letter, count in sorted_dictionary_asc.items():
-                print(f"{letter.upper()}: {count}")
-            print("------------------------ END ----------------------------")
-def main():
-    print("====================== BOOKBOT Frankenstein; Or, The Modern Prometheus ==========================")
-    get_book_text("books/frankenstein.txt")
-    return
+        print("------------------ Word Count ----------------------")
+        print(f"Found {len(get_word_count(filepath))} total words")
+        print("------------------ Character Count ----------------------")
+        chars_count = {}
+        chars_count = get_chars_count(filepath)
+        sorted_chars = sort_chars(chars_count)
 
+        for letter, count in sorted_chars.items():
+            print(f"{letter}: {count}")
+        print("------------------------ END ----------------------------")
+
+def main():
+    print("====================== BOOKBOT ==========================")
+    get_book_text(argv[1])
+    return
 main()
